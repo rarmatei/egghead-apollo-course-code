b3252b340fea39e983ccf8cf0263d5e83915369d
Rares Matei <matei.rar@gmail.com>
2021-08-07 22:18:11 +0100

    applies to all: note ui cleanup

--- client/src/shared-ui/UiNote.js
+++ client/src/shared-ui/UiNote.js
@@ -1,4 +1,4 @@
-import { Box, Flex, Text } from "@chakra-ui/react";
+import { Box, Flex, Stack, Text } from "@chakra-ui/react";
 
 export function UiNote({ isSelected, content, category, children }) {
   return (
@@ -6,11 +6,11 @@ export function UiNote({ isSelected, content, category, children }) {
       background={isSelected ? "#EDFDFD" : ""}
       p={2}
       shadow="md"
-      height={100}
+      minH={100}
       borderWidth="3px"
     >
-      <Flex h="100%" justify="space-between" marginBottom="10px">
-        <Flex flex="1" h="100%" justify="space-around" direction="column">
+      <Flex h="100%" justify="space-between">
+        <Stack>
           <Text>{content}</Text>
           {category && (
             <Flex>
@@ -20,10 +20,8 @@ export function UiNote({ isSelected, content, category, children }) {
               <Text>{category}</Text>
             </Flex>
           )}
-        </Flex>
-        <Flex justify="space-around" height="100%" direction="column">
-          {children}
-        </Flex>
+        </Stack>
+        <Stack>{children}</Stack>
       </Flex>
     </Box>
   );
