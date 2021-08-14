4a3842efd934fd6aa276b93327cd0557100ac99d
Rares Matei <matei.rar@gmail.com>
2021-08-14 13:23:10 +0100

    remove main notes container

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,24 @@
-import { MainNotesContainer } from "./MainNotesContainer";
 import { Route } from "react-router-dom";
 import { EditNote } from "./EditNote";
 import { UiAppLayout } from "./shared-ui/UiAppLayout";
+import { useState } from "react";
+import { Stack } from "@chakra-ui/react";
+import { SelectCategory } from "./SelectCategory";
+import { EditCategories } from "./EditCategories";
+import { NoteList } from "./NoteList";
 
 function App() {
+  const [notesCategory, setNotesCategory] = useState("1");
   return (
     <UiAppLayout>
-      <MainNotesContainer />
+      <Stack width={400}>
+        <SelectCategory
+          value={notesCategory}
+          onCategoryChange={(categoryId) => setNotesCategory(categoryId)}
+        />
+        <EditCategories />
+        <NoteList categoryId={notesCategory} />
+      </Stack>
       <Route path={`/note/:noteId`}>
         <EditNote />
       </Route>
