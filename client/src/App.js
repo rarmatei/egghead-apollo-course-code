cc64746e70358f487595035fd14384443ec2589e
Rares Matei <matei.rar@gmail.com>
2021-08-14 13:20:50 +0100

    reorganise app layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,16 @@
-import { Box, Flex } from "@chakra-ui/react";
 import { MainNotesContainer } from "./MainNotesContainer";
-import { UiAppMainHeader } from "./shared-ui/UiAppMainHeader";
 import { Route } from "react-router-dom";
 import { EditNote } from "./EditNote";
+import { UiAppLayout } from "./shared-ui/UiAppLayout";
 
 function App() {
   return (
-    <>
-      <UiAppMainHeader />
-      <Flex padding="10px" alignItems="flex-start">
-        <Box>
-          <MainNotesContainer />
-        </Box>
-        <Box paddingLeft="30px" width="450px">
-          <Route path={`/note/:noteId`}>
-            <EditNote />
-          </Route>
-        </Box>
-      </Flex>
-    </>
+    <UiAppLayout>
+      <MainNotesContainer />
+      <Route path={`/note/:noteId`}>
+        <EditNote />
+      </Route>
+    </UiAppLayout>
   );
 }
 
